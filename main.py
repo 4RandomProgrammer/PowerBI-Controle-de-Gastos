b387d1b363a7309ef1f686854b1a6f3a7e637ee5
luis.henriques1 <luis.henriques@dcomp.sor.ufscar.br>
2022-07-15 20:38:37 -0300

    New Code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 import pandas as pd
 import datetime
 
-planilha = None
+
 
 #Função responsável por inicializar o programa
 def init():
@@ -14,28 +14,38 @@ def init():
     try: 
         planilha = pd.read_excel(r'./Controle de Gastos.xlsx')
 
-    except FileNotFoundError as error:
+
+    except:
         
         #Caso a planilha não exista, cria-la vazia.
         print('Planilha não foi encontrada portanto criando uma nova planilha')
         planilha = pd.DataFrame(columns=['Data','Com o que foi gasto','Dinheiro Gasto'])
 
         update()
+    
+    return planilha
+
 
 
 #Função para adicionar uma linha DataFrame:
-def addRow(date, rotulo, quantia):
+def addRow(planilha ,date, rotulo, quantia):
+
     addedRow = {
-        'Data':date,
+        'Data': date.strftime("%x"),
         'Com o que foi gasto': rotulo,
         'Dinheiro Gasto': quantia
     }
 
     try:
-        planilha = planilha.append(addedRow)
+        planilha = planilha.append(addedRow,ignore_index=True)
         update()
-    except:
+
+    except Exception as e:
+        print(e)
         print('Não foi possivel adicionar uma linha.')
+
+    else:
+        print("Linha adcionada com sucesso!")
 
 #Função para escrever a planilha do execel
 def update():
@@ -46,41 +56,51 @@ def update():
     
 
 #Main
-init()
+if __name__ == "__main__":
+    
+    planilha = init()
+
+    while True:
+
+        print('O que você deseja fazer?')
+        print('1 - Adicionar linha de Gastos')
+        print('2 - Remover coluna')
+        print('3 - Abrir dashboard')
+        print('4 - Sair do programa')
+
+        try:
+            read = int(input())
+            
+            if read == 1:
+                while True:
+                    try:
+                        quantia = float(input("Digite o valor gasto: "))
+
+                        rotulo = str(input("Digite com o que foi gasto: "))
+
+                        dia = int(input("Digite o dia: "))
+                        mes = int(input("Digite o mes: "))
+                        ano = int(input("Digite o ano: "))
+
+                        data = datetime.date(ano,mes,dia)
 
 
-while True:
+                        addRow(planilha,data, rotulo, quantia)
 
-    print('O que você deseja fazer?')
-    print('1 - Adicionar linha de Gastos')
-    print('2 - Sair do programa')
 
-    try:
-        read = int(input())
-        
-        if read == 1:
-            while True:
-                try:
-                    quantia = float(input("Digite o valor gasto: "))
+                    except Exception as e:
+                        print(e)
+                        print("Valor Inválido!")
 
-                    rotulo = str(input("Digite com o que foi gasto: "))
-
-                    dia = int(input("Digite o dia: "))
-                    mes = int(input("Digite o mes: "))
-                    ano = int(input("Digite o ano: "))
-
-                    data = datetime.date(ano,mes,dia)
-
-                    addRow(data, rotulo, quantia)
-
-                    if True:
+                    else:
                         break
 
-                except:
-                    print("Valor Inválido!")
 
-        if read == 2:
-            print('Obrigado por utilizar o programa!')
-            break
-    except:
-        print('Opção Inválida tente novamente!')
+            elif read == 4:
+                print('Obrigado por utilizar o programa!')
+                break
+            else:
+                print("Opção ainda não implementada KK")
+
+        except:
+            print('Opção Inválida tente novamente!')
